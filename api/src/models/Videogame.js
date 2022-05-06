1db0b9b6d8093bf6ff5d97cb82e232c7d2afec48
Pablo Pelardas <pablo.pelardas@gmail.com>
2022-05-06 20:19:39 -0300

    [UPDATE] Backend Models and Associations]

--- api/src/models/Videogame.js
+++ api/src/models/Videogame.js
@@ -2,11 +2,36 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-  // defino el modelo
-  sequelize.define('videogame', {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-  });
+	// defino el modelo
+	sequelize.define('videogame', {
+		id: {
+			type: DataTypes.UUID,
+			primaryKey: true,
+			defaultValue: DataTypes.UUIDV4,
+		},
+		name: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		description: {
+			type: DataTypes.STRING,
+			allowNull: false,
+		},
+		launch_date: {
+			type: DataTypes.DATEONLY,
+		},
+		rating: {
+			type: DataTypes.FLOAT,
+		},
+		platforms: {
+			type: DataTypes.STRING,
+			allowNull: false,
+			get() {
+				return this.getDataValue('favColors').split(';');
+			},
+			set(val) {
+				this.setDataValue('favColors', val.join(';'));
+			},
+		},
+	});
 };
